ef676c2344f4566d31f983e703f8cd31c383d257
leeboa2005 <leeboa2005@naver.com>
2024-06-10 15:36:17 +0900

    todolist 중간 저장

--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,15 @@
 // TODO: todoSlice 를 작성하세요.
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [],
+};
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {},
+});
+
+export const {} = todoSlice.actions;
+export default todoSlice.reducer;
